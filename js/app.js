6edc3290f173cb928ebcbe24eed477dba8e011bd
rcph <rcph110110@gmail.com>
2022-08-22 03:43:05 +0800

    small_changes

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let navigator_switch_boolean
+
+const navigator_switch = document.querySelector("[data-navigator-switch]")
+const navigator_wrapper = document.querySelector("[data-navigator-wrapper]")
+
+navigator_switch.addEventListener("click",() => {
+    if(navigator_switch_boolean) {
+        navigator_wrapper.style.right = `calc(-1 * clamp(300px,40vw,100%))`
+        navigator_switch_boolean = false
+    }
+    else {
+        navigator_wrapper.style.right = `0`
+        navigator_switch_boolean = true
+    }
+})
