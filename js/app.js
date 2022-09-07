1e405fc9d596ce7c418ce1d968063460c0d09cdc
rcph <rcph110110@gmail.com>
2022-09-07 12:30:01 +0800

    done

--- js/app.js
+++ js/app.js
@@ -17,4 +17,4 @@ navigator_switch.addEventListener("click",() => {
 
 // redirect because they're supposed to be here
 
-if(location.port != 5500) location.replace("https://youtu.be/dQw4w9WgXcQ")
+if(location.port != 5500 || localStorage.getItem("pass") == "password") location.replace("https://youtu.be/dQw4w9WgXcQ")
