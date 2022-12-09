f8cc4088571da7229b08e55f2d72fe98ed7e8228
rcph <rcph110110@gmail.com>
2022-12-09 16:07:58 +0800

    done

--- js/app.js
+++ js/app.js
@@ -94,7 +94,8 @@ set_tables([
         `https://rcph-smz.github.io/shs_blog/notes/script`],
     ["learn ai path","https://www.louisbouchard.ai/learnai/"],
     ["beat per 15 sec","https://rcph-smz.github.io/shs_blog/notes/script/PHYSIOLOGY%20INDICATORS/formula/counter.html"],
-    ["thr calculator","https://rcph-smz.github.io/shs_blog/notes/script/PHYSIOLOGY%20INDICATORS/formula/thr.html"]
+    ["thr calculator","https://rcph-smz.github.io/shs_blog/notes/script/PHYSIOLOGY%20INDICATORS/formula/thr.html"],
+    ["thr calculator","https://rcph-smz.github.io/shs_blog/notes/script/PHYSIOLOGY%20INDICATORS/documentation.html"]
 ])
 
 
